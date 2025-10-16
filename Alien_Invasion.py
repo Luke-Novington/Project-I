33c20a2a4c12f4c7f05d43950fb1181e98743c89
Novington <casethat95@gmail.com>
2025-10-16 14:28:46 -0500

    1.2 We have movement bby

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -13,16 +13,34 @@ class Alieninvasion():
 
     def run_game(self):
         while True:
-            ### watch keyboard & mouse movements
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            pygame.display.flip()
+            self.check_events()
+            self.ship.update()
+            self.update_screen()
             self.clock.tick(60)
+
+    def check_events(self):
+        ### watch keyboard & mouse movements
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: ### Checks for events 
+                sys.exit()
+            elif event.type == pygame.KEYDOWN: ### checks for keyboard events such as a key being pressed
+                if event.key == pygame.K_d:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_a:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_a:
+                    self.ship.moving_left = False 
+                
+
+    def update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     ai = Alieninvasion()
