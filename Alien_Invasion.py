1dfd74f5400293d07ec87ed0ea063f366cc6739b
Novington <casethat95@gmail.com>
2025-10-15 22:51:43 -0500

    1.1 trying to add ship

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -1,12 +1,15 @@
 import pygame, sys
+from settings import Settings
+from ship import Ship
 
 class Alieninvasion():
     ### Overall class to manage game assets and behavior
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_length)
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -14,6 +17,9 @@ class Alieninvasion():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
             self.clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    ### class to store all settings for alien invasion
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_length = 800
+        self.bg_color = (230, 230, 230)
+
